94d8ff073c168ac96ca8d017d569cd47571656a1
Klepnev <klepnev-ea@yandex.ru>
2017-10-20 19:13:19 +0300

    Верстка

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-	$('#map').on('hover',function() {console.log('!!!')});
+	
+	// Гармошка
+	$('.cb-questions__link').on('click' , function(e) {
+		e.preventDefault();
+		var thisEl = $(this),
+			thisClosest = thisEl.closest('.cb-questions__elem');
+			$('.cb-questions__elem').removeClass('active');
+			thisClosest.addClass('active');
+	});
+
+	// подсветка карты
+	var svg = document.querySelector("svg");
+	var rects = document.querySelectorAll(".cb-map__redion");
+
+	var i = rects.length;
+	while(i--) {
+	  rects[i].addEventListener("mouseenter", function(e) {
+	    svg.appendChild(e.target);
+	    svg.appendChild(e.target);
+	  });
+	}
+
 	
 }); 
 
