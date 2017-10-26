6b6eb55ab60ed3cafe7bc635cad413a86f83b14f
Romuald <rarsenev@usetech.ru>
2017-10-26 08:47:22 +0300

    изменения в дизайне

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -37,23 +37,39 @@ $(document).ready(function() {
 
 			if(thisChildCekbox.prop("checked")) {  
 	            thisChildCekbox.prop('checked', false);
+	           // $(this).parent('.cb-interview__elem').addClass('active');
 	        } else {
 	            thisChildCekbox.prop('checked', true);
+	            // $(this).parent('.cb-interview__elem').removeClass('active');
 	        }
 	});
 
+	$('.cb-interview__elem').click(function() {
+		$(this).toggleClass('active');
+	});
 
 	// чекбокс карты
 
-	$('.btn_transparent').on('click', function(e) {
-		e.preventDefault();
-		e.stopPropagation();
-		var thisEl = $(this);
+	// $('.btn_transparent').on('click', function(e) {
+	// 	e.preventDefault();
+	// 	e.stopPropagation();
+	// 	var thisEl = $(this);
 
 
-		$('.btn-block-checkbox__check').css('left', thisEl.position().left);
-		$('.btn-block-checkbox__check').css('width', thisEl.css('width'));
-	});
+	// 	$('.btn-block-checkbox__check').css('left', thisEl.position().left);
+	// 	$('.btn-block-checkbox__check').css('width', thisEl.css('width'));
+	// });
+
+	
+	
+
+	// Верхнее меню
+	$('.cb-header__link').click(function() {
+		console.log('Пункт меню');
+		$('.cb-header__item').removeClass('active');
+		$(this).parent().addClass('active'); 
+
+	}); 
 	
 }); 
 
